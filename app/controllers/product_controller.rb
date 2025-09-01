46d7404e54e7527bfc69d9810f334b9c0b63e747
Hussein6685 <husseinail6685@gmail.com>
2025-09-01 16:48:00 +0300

    make small tweaks in the project

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,6 @@
 class ProductController < ApplicationController
   def index
     @product = Product.find(params[:id])
+    @product_reviews = @product.invoices.map { |invoice| invoice.reviews }
   end
 end
